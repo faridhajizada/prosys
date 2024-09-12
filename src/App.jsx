2f283e3469f101918ee39a1134f9d061405f67f4
Hajizada Farid <faridmh@code.edu.az>
2024-09-13 02:14:21 +0400

    feat: add Loading component and handle not found routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Headers";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Loading from "./components/Loading/Loading";
 
 const Lesson = lazy(() => import("./page/Lesson/Lesson"));
 const Student = lazy(() => import("./page/Student/Student"));
@@ -14,12 +15,13 @@ function App() {
         <Sidebar />
         <div className="main-content">
           <Header />
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Loading />}>
             <Routes>
               <Route path="/" element={<Student />} />
               <Route path="/lessons" element={<Lesson />} />
               <Route path="/students" element={<Student />} />
               <Route path="/exams" element={<Exam />} />
+              <Route path="*" element={<div>Not Found</div>} />
             </Routes>
           </Suspense>
         </div>
